e3a79c799cb21322225a51210bf5241125166f46
mahmadraza272-sketch <mahmadraza272@gmail.com>
2026-07-20 16:50:37 -0700

    Create 01_hello_world.dart

---
+++ 01_hello_world.dart
@@ -0,0 +1,7 @@
+void main() {
+  print("Hello, World!\nHello Dart!");
+
+  print("My name is Muhammad Ahmad Raza");
+  print("I am learning Dart");
+  print("My goal is to become a Flutter Developer");
+}
