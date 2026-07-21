b8ebc7b509a689c3d7b214e5bc95619c85d24665
mahmadraza272-sketch <mahmadraza272@gmail.com>
2026-07-20 19:58:09 -0700

    Create 03_data_types.dart

---
+++ 03_data_types.dart
@@ -0,0 +1,17 @@
+void main() {
+  // String
+  String name = "Ahmad";
+  // Integer
+  int age = 20;
+  // Double
+  double height = 5.8;
+  // Boolean
+  bool isStudent = true;
+  // List
+  List<String> skills = ["Dart", "Flutter", "C++"];
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Is Student: $isStudent");
+  print("Skills: $skills");
+}
