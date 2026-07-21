416445d661d383097f49469fab7ee8eff06fcffa
mahmadraza272-sketch <mahmadraza272@gmail.com>
2026-07-20 19:54:53 -0700

    Create 02_variables.dart

---
+++ 02_variables.dart
@@ -0,0 +1,15 @@
+void main() {
+  
+  var name = "Ahmad";
+
+  final country = "Pakistan";
+
+  const pi = 3.14159;
+
+  print("Name: $name");
+  print("Country: $country");
+  print("PI: $pi");
+  name = "Muhammad Ahmad Raza";
+
+  print("Updated Name: $name");
+}
