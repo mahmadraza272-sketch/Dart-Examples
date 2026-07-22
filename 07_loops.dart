a713abae621ac556e8378eef743c84a84ff543d1
mahmadraza272-sketch <mahmadraza272@gmail.com>
2026-07-21 18:33:49 -0700

    Create 07_loops.dart

---
+++ 07_loops.dart
@@ -0,0 +1,28 @@
+void main() {
+  // For Loop
+  print("For Loop:");
+
+  for (int i = 1; i <= 5; i++) {
+    print(i);
+  }
+
+  // While Loop
+  print("\nWhile Loop:");
+
+  int number = 1;
+
+  while (number <= 5) {
+    print(number);
+    number++;
+  }
+
+  // Do-While Loop
+  print("\nDo-While Loop:");
+
+  int count = 1;
+
+  do {
+    print(count);
+    count++;
+  } while (count <= 5);
+}
