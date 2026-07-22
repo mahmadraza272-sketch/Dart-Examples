c4a4b4315ac037f4a4c8f67828ac803933e6963a
mahmadraza272-sketch <mahmadraza272@gmail.com>
2026-07-21 18:06:42 -0700

    Create 06_conditions.dart

---
+++ 06_conditions.dart
@@ -0,0 +1,15 @@
+void main() {
+  int marks = 85;
+
+  if (marks >= 80) {
+    print("Grade: A");
+  } else if (marks >= 70) {
+    print("Grade: B");
+  } else if (marks >= 60) {
+    print("Grade: C");
+  } else if (marks >= 50) {
+    print("Grade: D");
+  } else {
+    print("Fail");
+  }
+}
